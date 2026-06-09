3834ba1591d3867fc1ef23e8a4e6512ddc958e42
Rahul Jain <rahuljain14314@gmail.com>
2026-06-09 15:41:12 +0530

    proxy bypass using corsProxy io

--- src/components/Body.js
+++ src/components/Body.js
@@ -5,55 +5,66 @@ import shimmer from './Shimmer.JS';
 import {useState, useEffect} from 'react';
 
 const Body = ({searchText}) => {
-    const [allRestaurants, setAllRestaurants] = useState([]);
-    const [listOfRestaurants, setListOfRestaurants] = useState([]);
-    const [isFiltered, setIsFiltered] = useState(false);
+    const [allFetchedRestaurants, setAllFetchedRestaurants] = useState([]);
+
+    const [displayedRestaurants, setDisplayedRestaurants] = useState([]);
+
+    const [isTopRatedFilterEnabled, setIsTopRatedFilterEnabled] =
+        useState(false);
 
     useEffect(() => {
-        fetchRestaurants();
+        fetchRestaurantDataFromSwiggy();
     }, []);
 
-    const fetchRestaurants = async () => {
+    const fetchRestaurantDataFromSwiggy = async () => {
         try {
             const response = await fetch(SWIGGY_API_URL);
-            const data = await response.json();
 
-            const restaurants =
-                data.data.cards[1].card.card.gridElements.infoWithStyle
+            const jsonResponse = await response.json();
+
+            const fetchedRestaurants =
+                jsonResponse.data.cards[1].card.card.gridElements.infoWithStyle
                     .restaurants;
 
-            setAllRestaurants(restaurants);
-            setListOfRestaurants(restaurants);
+            setAllFetchedRestaurants(fetchedRestaurants);
+
+            setDisplayedRestaurants(fetchedRestaurants);
         } catch (error) {
             console.error('Error fetching Swiggy data:', error);
         }
     };
 
-    const toggleTopRatedFilter = () => {
-        if (isFiltered) {
-            setListOfRestaurants(allRestaurants);
+    const toggleTopRatedRestaurantsFilter = () => {
+        if (isTopRatedFilterEnabled) {
+            setDisplayedRestaurants(allFetchedRestaurants);
         } else {
-            const filteredRestaurants = allRestaurants.filter(
-                (item) => item.info.avgRating > 4.2,
+            const topRatedRestaurants = allFetchedRestaurants.filter(
+                (restaurant) => restaurant.info.avgRating > 4.2,
             );
 
-            setListOfRestaurants(filteredRestaurants);
+            setDisplayedRestaurants(topRatedRestaurants);
         }
 
-        setIsFiltered(!isFiltered);
+        setIsTopRatedFilterEnabled(!isTopRatedFilterEnabled);
     };
 
-    const filteredRestaurants = listOfRestaurants.filter((restaurant) =>
-        restaurant.info.name.toLowerCase().includes(searchText.toLowerCase()),
+    const searchFilteredRestaurants = displayedRestaurants.filter(
+        (restaurant) =>
+            restaurant.info.name
+                .toLowerCase()
+                .includes(searchText.toLowerCase()),
     );
 
-    return listOfRestaurants.length === 0 ? (
+    return displayedRestaurants.length === 0 ? (
         shimmer()
     ) : (
         <div className='body'>
             <div className='filter'>
-                <button className='filter-btn' onClick={toggleTopRatedFilter}>
-                    {isFiltered
+                <button
+                    className='filter-btn'
+                    onClick={toggleTopRatedRestaurantsFilter}
+                >
+                    {isTopRatedFilterEnabled
                         ? 'Show All Restaurants'
                         : 'Top Rated Restaurants'}
                 </button>
@@ -64,8 +75,11 @@ const Body = ({searchText}) => {
             </div>
 
             <div className='restraurant-container'>
-                {filteredRestaurants.map((item) => (
-                    <RestaurantCard key={item.info.id} resd={item} />
+                {searchFilteredRestaurants.map((restaurant) => (
+                    <RestaurantCard
+                        key={restaurant.info.id}
+                        resd={restaurant}
+                    />
                 ))}
             </div>
         </div>
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -5,7 +5,7 @@ const APPLOGO =
     'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT5XJkLEIi2MGgOpToU2nZWzSgkTYj6io-LJdTwcFF-pw&s';
 
 const SWIGGY_API_URL =
-    'https://www.swiggy.com/dapi/restaurants/list/v5?lat=22.720542&lng=75.8419112&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING';
+    'https://corsproxy.io/?https://www.swiggy.com/dapi/restaurants/list/v5?lat=22.720542&lng=75.8419112&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING';
 
 const RATING_STAR = new URL('../../public/rating.png', import.meta.url).href;
 
